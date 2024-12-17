8733fc94f86bfb341a4abff91b567dc01363afe7
Kamil Antos <kamil.antos@fuelprime.com>
2024-12-17 17:44:23 +0100

    Init

---
+++ Assets/Scripts/ItemsPack/SO/SoItem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace ItemsPack.SO
+{
+    [CreateAssetMenu(menuName = "Custom/Item", fileName = "new Item")]
+    public class SoItem : ScriptableObject
+    {
+        [SerializeField] private string itemName;
+        [SerializeField] private Sprite itemSprite;
+        [SerializeField] private int itemWeight;
+
+        public string ItemName => itemName;
+        public Sprite ItemSprite => itemSprite;
+        public int ItemWeight => itemWeight;
+    }
+}
